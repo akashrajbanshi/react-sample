d3d989a6b0c27629e38f98d4c9ef57ecdf259c7e
akashrajbanshi <akashrajbanshi@hotmail.com>
2021-02-03 17:59:33 +0545

    Initial Commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+//Stateless Functional Component
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <span className="badge badge-pill badge-secondary m-2">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
